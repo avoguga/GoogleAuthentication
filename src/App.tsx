21cb494734bcbb7f21580f2ac31adbbb2528f3c9
O Gustavo <avoguga@gmail.com>
2022-03-16 02:44:43 -0300

    testing auth with localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { signInWithGoogle } from "./Firebase";
 
 function App() {
+  const aa: any = localStorage.getItem("profilePic");
+  const bb: any = localStorage.getItem("name");
+  const cc: any = localStorage.getItem("email");
 
   return (
     <div className="App">
-      <h1>Hi mommy</h1>
+      <button onClick={signInWithGoogle}>Sign In</button>
+
+      <h1>{bb}</h1>
+      <h1>{cc}</h1>
+      <img src={aa} alt="" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
